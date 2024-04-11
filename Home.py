ac204ffaa783dd285f5517d8675c61626fc41f72
song9537 <140922961+song9537@users.noreply.github.com>
2024-04-10 22:56:10 -0300

    Streamlit setup with different app selection

---
+++ Home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FullStackGPT Home",
+    page_icon="🤖",
+)
+
+st.markdown("""
+# Hello!
+
+Welcome to my Generative AI Portfolio!
+
+Using Langchain and Streamlit, there are the apps I made using GPT API key:
+
+- [ ] [DocumentGPT]( /DocumentGPT)
+- [ ] [PrivateGPT]( /PrivateGPT)
+- [ ] [QuizGPT]( /QuizGPT)
+- [ ] [SiteGPT]( /SiteGPT)
+- [ ] [MeetingGPT]( /MeetingGPT)
+- [ ] [InvestorGPT]( /InvestorGPT)
+
+Please enjoy!
+      
+""")
