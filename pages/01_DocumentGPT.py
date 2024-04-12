2288a7a02026377885a625712144896fe7031153
song9537 <140922961+song9537@users.noreply.github.com>
2024-04-12 01:12:17 -0300

    added llm chain to document GPT needs memeroy system

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,14 +1,41 @@
-import time
+from typing import Any, Dict, List
+from uuid import UUID
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📄",
+)
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    
+    message = ""
+    
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+            
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+            
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ]
 )
 
 if "messages" not in st.session_state:
@@ -34,15 +61,34 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+def save_message(message, role):
+    st.session_state["messages"].append({"message":message, "role":role})
+
 def send_message(message,role, save=True):
     with st.chat_message(role):
         st.markdown(message)
     if save:
-        st.session_state["messages"].append({"message":message, "role":role})
+        save_message(message,role)
+
 
 def paint_history():
     for message in st.session_state["messages"]:
         send_message(message["message"], message["role"], save=False)
+        
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+prompt = ChatPromptTemplate.from_messages([
+    (
+        "system", 
+        """
+        Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything  up.
+        
+        Context: {context}     
+        """,
+     ),
+    ("human", "{question}"),
+])
 
 st.title("Document GPT")
 
@@ -65,5 +111,13 @@ if file:
     
     if message:
         send_message(message,"human")
+        
+        chain = ({
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough()
+        } | prompt | llm)
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
+
 else:
     st.session_state["messages"] = []
